a981c32f2638c4929f2d22262390bff5b0857694
Sinh Phan <pnsinh.hg92@gmail.com>
2023-08-27 15:04:18 +0700

    remove log

--- stock_util/overrides.py
+++ stock_util/overrides.py
@@ -6,5 +6,4 @@ from erpnext.stock.doctype.stock_entry.stock_entry import StockEntry
 
 class CustomStockEntry(StockEntry):
     def validate_qty(self):
-        frappe.throw(_("--->>>>CustomStockEntry:validate_qty---1").format())
         pass
