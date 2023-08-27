352c5f5029a9867e10a4009c197f3b272dc4ac0a
Sinh Phan <pnsinh.hg92@gmail.com>
2023-08-27 15:00:38 +0700

    up

--- stock_util/overrides.py
+++ stock_util/overrides.py
@@ -6,5 +6,5 @@ from erpnext.stock.doctype.stock_entry.stock_entry import StockEntry
 
 class CustomStockEntry(StockEntry):
     def validate_qty(self):
-        frappe.throw(_("--->>>>CustomStockEntry:validate_qty").format())
+        frappe.throw(_("--->>>>CustomStockEntry:validate_qty---1").format())
         pass
